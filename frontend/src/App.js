8c4177ea894488d94507f9a4e60ee98f38de0a5d
poornae <poornae_9611@hotmail.com>
2023-09-23 21:30:40 +0530

    ML model

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
+import EmotionDetector from './frontend/components/EmotionDetector/EmotionDetector';
 
 function App() {
   const [songs, setSongs] = useState([]);
