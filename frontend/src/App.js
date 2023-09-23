8221c979352826c78fd4d3b205d5238b8a6de43f
poornae <poornae_9611@hotmail.com>
2023-09-23 15:36:43 +0530

    Initial commit for Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+  const fetchBackend = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data from backend:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EmoTunes</h1>
+      <button onClick={fetchBackend}>Test Backend</button>
     </div>
   );
 }
